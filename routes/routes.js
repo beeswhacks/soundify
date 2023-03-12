bf5db2c053ae56fb26e9b7afc19bff1ce62d4949
Jack Preston <jack.preston@smg.team>
2023-03-12 20:45:16 +0000

    feat: create playlist based on bbc sounds tracklist

--- routes/routes.js
+++ routes/routes.js
@@ -13,9 +13,23 @@ const client_secret = process.env.CLIENT_SECRET || null;
 const spotify_accounts_base_url = 'https://accounts.spotify.com/';
 const spotify_api_base_url = 'https://api.spotify.com/v1/';
 
+function getUserInfo(access_token) {
+    return (
+        axios({
+            baseURL: spotify_api_base_url,
+            url: '/me',
+            method: 'get',
+            headers: {
+                'Authorization': 'Bearer ' + access_token,
+                'Content-Type': 'application/json',
+            },
+        })
+    );
+}
+
 // authorise user through Spotify API
 router.get('/api/login', cors(), (req, res) => {
-    const scope = 'user-read-private user-read-email';
+    const scope = 'user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public';
 
     // redirect login request to Spotify API
     res.redirect(spotify_accounts_base_url + 'authorize?' +
@@ -68,7 +82,7 @@ router.get('/api/loginRedirect', cors(), async (req, res) => {
                 .cookie('access_token_granted', true, {
                     maxAge: maxCookieAgeInSeconds,
                 })
-                .redirect(`/api/getUserInfo/${maxCookieAgeInSeconds}`);
+                .redirect(`/api/getUserName/${maxCookieAgeInSeconds}`);
         } else {
             res.cookie('access_token_granted', false).redirect('/');
         }
@@ -79,22 +93,13 @@ router.get('/api/loginRedirect', cors(), async (req, res) => {
     }
 });
 
-router.get('/api/getUserInfo/:maxCookieAge', cors(), async (req, res) => {
+router.get('/api/getUserName/:maxCookieAge', cors(), async (req, res) => {
     const access_token = req.cookies.access_token;
 
     if (!access_token) {
         res.status(500).send('An access token is required to get user info but none was provided.');
     } else {
-        const userInfo = await axios({
-            baseURL: spotify_api_base_url,
-            url: '/me',
-            method: 'get',
-            headers: {
-                'Authorization': 'Bearer ' + access_token,
-                'Content-Type': 'application/json',
-            }
-        });
-        // res.json(userInfo);
+        const userInfo = await getUserInfo(access_token);
         res.cookie('user_name', userInfo.data.display_name, {
             maxAge: req.params.maxCookieAge,
         })
@@ -103,20 +108,23 @@ router.get('/api/getUserInfo/:maxCookieAge', cors(), async (req, res) => {
 });
 
 router.post('/api/:showId', cors(), async (req, res) => {
+    const access_token = req.cookies.access_token;
 
     // let tracklist;
     const url = 'https://www.bbc.co.uk/sounds/play/' + req.params.showId;
 
     // scrape data from BBC Sounds
-    const bbcSoundsData = await JSDOM.fromURL(url, { resources: 'usable' })
+    const bbcSoundsData = await JSDOM
+        .fromURL(url, { resources: 'usable' })
         .then(dom => {
             const scripts = dom.window.document.querySelectorAll('body script');
             const searchText = 'window.__PRELOADED_STATE__ = '
+            // look for searchText in each script
             var isolatedScript = '';
             scripts.forEach(script => {
                 const trimmedScript = script.text.trim();
                 if (trimmedScript.slice(0, searchText.length) === searchText) {
-                    isolatedScript = trimmedScript.slice(searchText.length, -1);
+                    return isolatedScript = trimmedScript.slice(searchText.length, -1);
                 }
             });
             return isolatedScript;
@@ -124,26 +132,83 @@ router.post('/api/:showId', cors(), async (req, res) => {
         .then(isolatedScript => {
             scriptJSON = JSON.parse(isolatedScript);
             return scriptJSON;
-        })
-        .catch(err => {
-            console.error(err);
-            if (err) { res.sendStatus(500) };
         });
 
-    const tracklist = bbcSoundsData.tracklist;
-    console.log(JSON.stringify(bbcSoundsData, null, 2));
+    const tracklist = bbcSoundsData.tracklist.tracks;
 
-    // tracklist.tracks[1].uris[0].uri
+    // each track has a set of URIs pointing to the track on apple music and/or spotify
+    // go through the URIs for each track and only return spotify URIs
+    const spotifyUriObjects = tracklist.flatMap(track => {
+        return track.uris.filter(uri => uri.id === 'commercial-music-service-spotify');
+    });
+
+    // each uri is a link of the form https://open.spotify.com/track/<spotifyId>
+    // create new URL object for each uri and extract the last section of the href to get spotifyId
+    // the spotifyId identifies the spotify track so can be used to find the song and add it to a playlist
+    const spotifyUris = spotifyUriObjects.flatMap(uriObj => {
+        const uri = new URL(uriObj.uri);
+        const spotifyId = uri.href.substring(uri.href.lastIndexOf('/') + 1);
+        return 'spotify:track:' + spotifyId;
+    })
 
     // extract show title and release date for Spotify playlist title
+    const showTitle = bbcSoundsData.modules.data[0].data[0].titles.primary;
+    const releaseDate = new Date(bbcSoundsData.modules.data[0].data[0].release.date);
+    const playlistName = showTitle + ' - ' + releaseDate.toDateString();
 
     // get user's Spotify user_id
+    const userInfo = await getUserInfo(access_token);
+    const spotifyUserId = userInfo.data.id;
+
+    // check if user already has playlist for the show
+    const userPlaylists = await axios({
+        baseURL: spotify_api_base_url,
+        url: '/me/playlists',
+        method: 'get',
+        headers: {
+            'Authorization': 'Bearer ' + access_token,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const matchingPlaylists = userPlaylists.data.items.some(playlist => playlist.name == playlistName);
 
     // create spotify playlist from bbc sounds tracklist
+    if (matchingPlaylists === false) {
+        const newPlaylist = await axios({
+            baseURL: spotify_api_base_url,
+            url: `/users/${spotifyUserId}/playlists`,
+            method: 'post',
+            headers: {
+                'Authorization': 'Bearer ' + access_token,
+                'Content-Type': 'application/json',
+            },
+            data: {
+                "name": playlistName,
+                "description": "Auto-generated using Soundify",
+                "public": false,
+            }
+        });
 
-    // add songs to playlist
+        // add songs to playlist
+        const addTracksToPlaylist = await axios({
+            baseURL: spotify_api_base_url,
+            url: `/playlists/${newPlaylist.data.id}/tracks`,
+            method: 'post',
+            headers: {
+                'Authorization': 'Bearer ' + access_token,
+                'Content-Type': 'application/json',
+            },
+            data: {
+                "uris": spotifyUris,
+                "position": 0,
+            }
+        });
 
-    // send link to playlist to front end?
+        res
+        .cookie('playlist_url', newPlaylist.data.external_urls.spotify)
+        .redirect('/');
+    }
 });
 
 module.exports = router;
