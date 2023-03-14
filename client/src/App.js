062c2fd2368581544b382305975de0f2a2c5b323
Jack Preston <jack.preston@smg.team>
2023-03-14 19:30:46 +0000

    feat: check user connected to Spotify before creating playlist

--- client/src/App.js
+++ client/src/App.js
@@ -33,28 +33,33 @@ function URLGetter() {
         const form = e.target;
         const formData = new FormData(form);
 
-        try {
-            const url = new URL(formData.get('url'));
-            const showId = url.href.substring(url.href.lastIndexOf('/') + 1);
-            setFeedback('Generating playlist...');
+        if (!Cookies.get('access_token_granted')) {
+            setFeedback('It looks like you\'re not connected to Spotify. Connect to Spotify and try again.' );
+        } else {
+            try {
+                const url = new URL(formData.get('url'));
+                const showId = url.href.substring(url.href.lastIndexOf('/') + 1);
+                setFeedback('Generating playlist...');
 
-            fetch('/api/' + showId, {
-                method: 'POST'
-            })
-            .then(response => {
-                if (response.ok && Cookies.get('playlist_url')) {
-                    setFeedback('Done! Here\'s your playlist: ' + Cookies.get('playlist_url'));
-                } else {
-                    setFeedback('Unable to generate playlist. Please make sure that: the URL points to a real BBC Sounds show, that the show has a tracklist, and that the URL is formatted like: https://www.bbc.co.uk/sounds/play/<showId>')
-                }
-            })
-            .then(data => console.log(data));
-        } catch (error) {
-            setFeedback(
-                'The URL provided is incorrectly formatted. Make sure it is formatted like this: ' +
-                'https://www.bbc.co.uk/sounds/play/<showId>'
-            )
+                fetch('/api/' + showId, {
+                    method: 'POST'
+                })
+                .then(response => {
+                    if (response.ok && Cookies.get('playlist_url')) {
+                        setFeedback('Done! Here\'s your playlist: ' + Cookies.get('playlist_url'));
+                    } else {
+                        setFeedback('Unable to generate playlist. Please make sure that: the URL points to a real BBC Sounds show, that the show has a tracklist, and that the URL is formatted like: https://www.bbc.co.uk/sounds/play/<showId>')
+                    }
+                })
+                .then(data => console.log(data));
+            } catch (error) {
+                setFeedback(
+                    'The URL provided is incorrectly formatted. Make sure it is formatted like this: ' +
+                    'https://www.bbc.co.uk/sounds/play/<showId>'
+                )
+            }
         }
+
     }
 
     return (
