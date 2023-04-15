1b32635477f5490057fca81e6560bb58093669f5
Jack Preston <jack.preston@smg.team>
2023-04-15 08:38:31 +0100

    add full path to serve react app

--- routes.js
+++ routes.js
@@ -227,7 +227,7 @@ router.post('/api/:showId', cors(), async (req, res) => {
 });
 
 router.get('/*', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 module.exports = router;
